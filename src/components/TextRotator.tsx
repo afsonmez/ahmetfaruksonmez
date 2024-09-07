aa7fb47efe84639c78409586168341bcd938f221
afsonmez <ahmet2.faruk5@gmail.com>
2024-09-07 20:25:17 +0300

    v1.5

--- src/components/TextRotator.tsx
+++ src/components/TextRotator.tsx
@@ -6,17 +6,21 @@ function TextRotator() {
     <div
     className="py-4  rounded-md flex flex-col justify-center items-center overflow-hidden"
   >
-<div className="font-poppins text-base sm:text-3xl [text-wrap:balance] text-gray-700">
-  Psikoloji Öğrencisi<br />
-  <span
-    className="inline-flex ml-2 flex-col h-[calc(theme(fontSize.lg)*theme(lineHeight.tight))] sm:h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] overflow-hidden"
-  >
-    <span className="block text-left font-rubik text-lg sm:text-3xl leading-tight text-[#2f7df4]">
-      Grafik Tasarımcı
-    </span>
-  </span>
-</div>
+    <div className="font-poppins text-base sm:text-3xl [text-wrap:balance] text-gray-700">
+    Psikoloji Öğrencisi & <br />
+      <span
+        className="inline-flex ml-2 flex-col h-[calc(theme(fontSize.lg)*theme(lineHeight.tight))] sm:h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] overflow-hidden"
+      >
+        <ul
+          className="block text-left font-rubik text-lg sm:text-3xl leading-tight "
+        >
+          <li className="text-[#2f7df4]">Grafik Tasarımcı</li>
 
+          
+          
+        </ul>
+      </span>
+    </div>
   </div>
   )
 }
