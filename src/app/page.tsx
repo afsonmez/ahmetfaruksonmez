817ed4381f331568374044e6b16b9d8b81904aea
afsonmez <ahmet2.faruk5@gmail.com>
2024-09-08 00:39:46 +0300

    v1.9.6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import FramerWrapper from "@/components/FramerWrapper";
 export const siteConfig = {
   name: "Ahmet Faruk Sönmez",
   description: "Psikoloji öğrencisi ve Grafik Tasarımcı",
-  ogImage: "../../public/afso.png",
+ 
   url: "https://ahmetfaruksonmez.vercel.app/",
 }
 export default function Home() {
